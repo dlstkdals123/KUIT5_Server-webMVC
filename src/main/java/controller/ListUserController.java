7526b64481d50344fd0d375c22a3ccf684b8b4af
dlstkdals123 <dlstkdals123@naver.com>
2025-04-07 07:50:40 +0900

    Refactor: 불필요한 import 제거

--- src/main/java/controller/ListUserController.java
+++ src/main/java/controller/ListUserController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Enumeration;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
