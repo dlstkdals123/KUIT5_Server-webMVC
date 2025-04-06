9cf865334e4a4cf83190ae283ce5df1713e2ffc9
dlstkdals123 <dlstkdals123@naver.com>
2025-04-06 17:36:54 +0900

    요구사항 1-4: 로그인/로그아웃

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,28 @@
 package controller;
 
-public class LoginController {
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+
+        User user = MemoryUserRepository.getInstance().findUserById(userId);
+        if (user == null || !user.getPassword().equals(password))
+            resp.sendRedirect("/user/login_failed.jsp");
+
+        req.getSession().setAttribute("user", user);
+        req.getRequestDispatcher("/home.jsp").forward(req, resp);
+    }
 }
